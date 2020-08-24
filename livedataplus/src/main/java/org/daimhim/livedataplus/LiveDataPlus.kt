41be7275cb0f6cc6bfc3d94b5c259280d884c583
Daimhim <daimhim@163.com>
2020-08-25 01:32:32 +0800

    init

---
+++ livedataplus/src/main/java/org/daimhim/livedataplus/LiveDataPlus.kt
@@ -0,0 +1,36 @@
+package org.daimhim.livedataplus
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+
+class LiveDataPlus {
+
+
+//    fun observe(
+//        owner: LifecycleOwner,
+//        observer: Observer<in T?>
+//    )
+
+    abstract class ObserverWrapper<T> {
+        /**
+         * 是否活跃
+         */
+        abstract fun shouldBeActive(): Boolean
+
+        /**
+         * 更新
+         * newActive true 为最新数据
+         * false 为老数据版本同步
+         */
+        abstract fun activeStateChanged(newActive: Boolean)
+
+        /**
+         * 断开监听
+         */
+        abstract fun detachObserver()
+    }
+
+    interface IObserverWrapper<T>{
+
+    }
+}
