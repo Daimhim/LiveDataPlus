71e883ae4435180bd5de9fd08cee4016c7053263
Daimhim <daimhim@163.com>
2020-09-21 01:06:53 +0800

    init demo

--- app/src/main/java/org/daimhim/livedataplus/MainViewModel.kt
+++ app/src/main/java/org/daimhim/livedataplus/MainViewModel.kt
@@ -1,5 +1,17 @@
 package org.daimhim.livedataplus
 
-class MainViewModel
-{
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel{
+    var liveData = MutableLiveData<String>()
+
+    fun upData(){
+        GlobalScope.launch {
+            delay(1000)
+
+        }
+    }
 }
